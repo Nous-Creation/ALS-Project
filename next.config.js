01b7fd6b8466d92e3f3acef27535583ba661651c
tomoyanishi <nishi.tomoya.519@gmail.com>
2023-10-27 20:37:13 +0900

    update: text

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.BASE_PATH || "",
-  basePath: process.env.BASE_PATH || "",
   compiler: {
     styledComponents: true,
   },
